5da602e6dacb4a788ce1e3a57632465cdcc6b3dd
Andrew <andrey.kitaev55@gmail.com>
2022-01-13 19:46:27 +0300

    release 2

--- db/seeders/20220112141530-Services.js
+++ db/seeders/20220112141530-Services.js
@@ -1,24 +1,22 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     await queryInterface.bulkInsert('Services', [
-      {title: 'Фотосъемка', price: 10000, createdAt: new Date(), updatedAt: new Date()},
-      {title: 'Видеосъемка', price: 15000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Фотосессия в студии', price: 15000, createdAt: new Date(), updatedAt: new Date()},
       {title: 'Прямая трансляция', price: 10000, createdAt: new Date(), updatedAt: new Date()},
-      {title: 'Многокамерная видеосъемка', price: 30000, createdAt: new Date(), updatedAt: new Date()},
-  ], 
-  {});
-
+      {title: 'Многокамерная видеосъемка', price: 50000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Фотосессия стиле love story', price: 20000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Детская фотосъемка', price: 12000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Корпоративная фотосъемка', price: 30000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Видеосъемка', price: 15000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Профессиональный монтаж', price: 15000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Анимация', price: 15000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Озвучивание и музыка', price: 15000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Basic', price: 20000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Silver', price: 33000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Gold', price: 51000, createdAt: new Date(), updatedAt: new Date()},
+      {title: 'Platinum', price: 89000, createdAt: new Date(), updatedAt: new Date()},
+  ]);
+  },
   down: async (queryInterface, Sequelize) => {
     /**
      * Add commands to revert seed here.
@@ -26,5 +24,5 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+    }
 };
